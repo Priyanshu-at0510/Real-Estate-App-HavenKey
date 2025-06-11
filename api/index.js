da2904f1b4af0cfeb44f36bf95fe26fc59c8565e
priyanshu singh <ishusingh4006@gmail.com>
2025-06-11 16:58:16 +0530

    testing the default route after creating models ,routes and controllers

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import mongoose, { Mongoose } from "mongoose"
 import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js"
+
 dotenv.config();
 console.log("mongoDb URL",process.env.MONGODB_URL);
 mongoose.connect(process.env.MONGODB_URL, {
@@ -11,10 +13,14 @@ mongoose.connect(process.env.MONGODB_URL, {
 .catch((err) => console.error("MongoDB Connection Error:", err));
 
 const app=express();
-
 const PORT=3000;
+
+
 
 app.listen(PORT,()=>{
     console.log(`server started at PORT : ${PORT} !!! `);
 });
+
+app.use("/api/user",userRouter);
+
 
